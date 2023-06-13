811942d8f2c30533bf86215013a2f7e9fde38c0d
fares <faresrenassar@gmail.com>
2023-06-13 18:11:37 +0300

    edit project

--- Fa.js
+++ Fa.js
@@ -3,21 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const quoteContainer = document.getElementById("quoteContainer");
     let quotes = [];
   
-    // Fetch quotes from the API
     fetch("https://api.example.com/quotes")
       .then(response => response.json())
       .then(data => {
         quotes = data;
   
-        // Display all quotes initially
         displayQuotes(quotes);
   
-        // Add event listener for input changes
         searchInput.addEventListener("input", filterQuotes);
       })
       .catch(error => console.log(error));
   
-    // Display quotes
     function displayQuotes(quotesToShow) {
       quoteContainer.innerHTML = "";
       quotesToShow.forEach(quote => {
@@ -27,7 +23,6 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   
-    // Filter quotes based on search input
     function filterQuotes() {
       const searchTerm = searchInput.value.toLowerCase();
       const filteredQuotes = quotes.filter(quote => quote.text.toLowerCase().includes(searchTerm));
